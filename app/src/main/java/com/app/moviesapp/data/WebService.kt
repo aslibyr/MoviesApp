68cfe2db06980a48741b98d0e9105c99594a5ca1
Aslı <aslibyr00@gmail.com>
2024-03-06 14:23:35 +0300

    API added

---
+++ app/src/main/java/com/app/moviesapp/data/WebService.kt
@@ -0,0 +1,4 @@
+package com.app.moviesapp.data
+
+interface WebService {
+}
---
+++ app/src/main/java/com/app/moviesapp/utils/ScreenRoutes.kt
@@ -0,0 +1,14 @@
+package com.app.moviesapp.utils
+
+object ScreenRoutes {
+    const val BACK_PRESSED = "back_pressed"
+    const val CRYPTO_SCREEN_ROUTE = "crypto_screen"
+    const val NFT_HOST_ROUTE = "nft_host_route"
+    const val NFT_SCREEN_ROUTE = "nft_screen"
+    const val COLLECTION_DETAIL_SCREEN_ROUTE = "collection_detail_screen_route/{collectionAddress}"
+    const val CRYPTO_DETAIL_SCREEN_ROUTE = "crypto_detail_screen_route/{id}"
+    const val CRYPTO_HOST_ROUTE = "crypto_host_route"
+    const val NEWS_HOST_ROUTE = "news_host_route"
+    const val NEWS_SCREEN_ROUTE = "news_screen_route"
+    const val NEWS_DETAIL_SCREEN_ROUTE = "news_detail_screen_route/{id}"
+}
