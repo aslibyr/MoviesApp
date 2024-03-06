26f82b82fda6a5f53b1b46d0cdef642c03816e77
Aslı <aslibyr00@gmail.com>
2024-03-06 13:49:39 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+
+}
