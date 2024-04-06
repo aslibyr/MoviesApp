6e7fcaab5ced3f1103ab5c883012df1429a81fef
Aslı <aslibyr00@gmail.com>
2024-04-06 22:10:57 +0300

    Favorite person flow added.

--- app/src/main/java/com/app/moviesapp/data/local/MoviesAppDataBase.kt
+++ app/src/main/java/com/app/moviesapp/data/local/MoviesAppDataBase.kt
@@ -3,10 +3,13 @@ package com.app.moviesapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.app.moviesapp.data.local.dao.FavoriteMoviesDao
+import com.app.moviesapp.data.local.dao.FavoritePersonDao
 import com.app.moviesapp.data.local.entity.FavoriteMovieEntity
+import com.app.moviesapp.data.local.entity.FavoritePersonEntity
 
 
-@Database(entities = [FavoriteMovieEntity::class], version = 1)
+@Database(entities = [FavoriteMovieEntity::class, FavoritePersonEntity::class], version = 1)
 abstract class MoviesAppDataBase : RoomDatabase() {
     abstract fun favoriteMovies(): FavoriteMoviesDao
+    abstract fun favoritePerson(): FavoritePersonDao
 }
