8f6c967dd24e7a2e99590fbc654f1942ee8b48a1
Aslı <aslibyr00@gmail.com>
2024-04-14 17:17:59 +0300

    Bug fix

--- app/src/main/java/com/app/moviesapp/data/ui_models/MovieDetailUIModel.kt
+++ app/src/main/java/com/app/moviesapp/data/ui_models/MovieDetailUIModel.kt
@@ -1,10 +1,5 @@
 package com.app.moviesapp.data.ui_models
 
-import android.os.Parcelable
-import com.app.moviesapp.utils.Constant
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class MovieDetailUIModel(
     val title: String = "",
     val overview: String = "",
@@ -12,10 +7,4 @@ data class MovieDetailUIModel(
     val duration: String = "",
     val isFavorite: Boolean = false,
     val movieImage: String = ""
-) : Parcelable {
-    fun getImagePath(): String {
-        return Constant.BASE_POSTER_URL + movieImage
-    }
-}
-
-
+)
