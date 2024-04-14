9bafa762ff4b996ed103cffec5b014d677c0842e
Aslı <aslibyr00@gmail.com>
2024-04-14 18:24:59 +0300

    changed person mapper

--- app/src/main/java/com/app/moviesapp/data/ui_models/PersonUIModel.kt
+++ app/src/main/java/com/app/moviesapp/data/ui_models/PersonUIModel.kt
@@ -2,10 +2,8 @@ package com.app.moviesapp.data.ui_models
 
 data class PersonUIModel(
     val personId: Int = 0,
-    val birthday: String = "",
     val biography: String = "",
     val name: String = "",
-    val placeOfBirth: String = "",
     val profilePath: String = "",
     val isFavorite: Boolean = false
 )
